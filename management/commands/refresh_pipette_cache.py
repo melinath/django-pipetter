b30ea93cd8ab064247e28a5c84cb39f74f14c3b9
Stephen Burrows <stephen.r.burrows@gmail.com>
2010-09-07 15:23:24 -0400

    Fixed errors in pipette refresh script.

--- management/commands/refresh_pipette_cache.py
+++ management/commands/refresh_pipette_cache.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.urlresolvers import reverse
 from pipettes.utils import refresh_cache
 import urllib2
+from django.contrib.sites.models import Site
 
 
 class Command(BaseCommand):
@@ -9,6 +10,6 @@ class Command(BaseCommand):
 	help = 'Refreshes all current caches for the given pipettes.'
 	
 	def handle(self, *args, **options):
-		url = 'http://%s%s/' % (Site.objects.get_current().domain, reverse('pipettes.views.refresh_cache', kwargs={'pipette_names': '/'.join(args)}))
+		url = 'http://%s%s' % (Site.objects.get_current().domain, reverse('pipettes.views.refresh_cache', kwargs={'pipette_names': ''.join(['%s/' % arg for arg in args])}))
 		urllib2.urlopen(url)
-		self.stdout.write('Caches reset.')
+		self.stdout.write('Caches reset.\n\n')
