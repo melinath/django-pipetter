1e74b3f1b751f7076be9c20e3f7b300c1281fb22
Stephen Burrows <stephen.r.burrows@gmail.com>
2010-09-22 13:35:06 -0400

    Added timeout to noaa load for cases where weather.gov is down. Like this morning.

--- default_pipettes/noaa.py
+++ default_pipettes/noaa.py
@@ -16,7 +16,8 @@ else:
 		cache_for = 30
 		
 		def get_context(self, station, datakeys=['temp_f', 'temp_c', 'link', 'weather']):
-			page = urllib2.urlopen('http://www.weather.gov/xml/current_obs/%s.xml' % station)
+			# In case weather.gov is down, timeout after 1 second to prevent long page loads.
+			page = urllib2.urlopen('http://www.weather.gov/xml/current_obs/%s.xml' % station, None, 1)
 			soup = BeautifulSoup(page)
 			context = {}
 			for key in datakeys:
